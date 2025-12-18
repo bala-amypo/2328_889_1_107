22db615d30915bc23d053c27489f7d7247cdc024
bala-amypo <bala@amypo.in>
2025-12-18 09:09:48 +0000

    User commit

--- src/main/java/com/example/demo/model/Evidence.java
+++ src/main/java/com/example/demo/model/Evidence.java
@@ -1,5 +1,14 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Id;
+
 public class Evidence{
-    
+    @Id
+    private Long id;
+    private String IMAGE;
+    private String DOCUMENT;
+    private String TEXT;
+    private String fileUrl;
+    private
+
 }
