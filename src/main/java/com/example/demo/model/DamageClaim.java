231da63a15e152e3c6708cb061872e59b03555b1
bala-amypo <bala@amypo.in>
2025-12-18 08:54:46 +0000

    User commit

--- src/main/java/com/example/demo/model/DamageClaim.java
+++ src/main/java/com/example/demo/model/DamageClaim.java
@@ -1,1 +1,6 @@
 package com.example.demo.model;
+
+public class damageClaim{
+    @Id
+    private Long id;
+}
--- src/main/java/com/example/demo/model/Parcel.java
+++ src/main/java/com/example/demo/model/Parcel.java
@@ -13,5 +13,6 @@ public class Parcel{
     private String receiverName;
     @Min(1)
     private Double weightKg;
+    
     private LocalDate deliveredAt;
 }
