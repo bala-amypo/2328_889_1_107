5b19d519b706e2573cd8908e621e451ccf9a4818
bala-amypo <bala@amypo.in>
2025-12-18 08:44:48 +0000

    User commit

--- src/main/java/com/example/demo/model/Parcel.java
+++ src/main/java/com/example/demo/model/Parcel.java
@@ -1,8 +1,14 @@
 package com.example.demo.model;
 import jakarta.persistence.Id;
+import java.time.LocalDate;
 
 public class Parcel{
     @Id
     private Long id;
-    private String
+    @Column(unique = true)
+    private String trackingNumber;
+    private String senderName;
+    private String receiverName;
+    private double weightKg;
+    private LocalDate deliveredAt;
 }
