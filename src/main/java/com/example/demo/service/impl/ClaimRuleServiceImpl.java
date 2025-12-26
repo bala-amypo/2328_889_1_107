0f0d2bc17a5477130dec58ae2e7c9fa98b5ae685
bala-amypo <bala@amypo.in>
2025-12-26 04:59:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ClaimRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ClaimRuleServiceImpl.java
@@ -1,26 +1,32 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.ClaimRule;
 import com.example.demo.repository.ClaimRuleRepository;
 import com.example.demo.service.ClaimRuleService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ClaimRuleServiceImpl implements ClaimRuleService {
 
-    private final ClaimRuleRepository repo;
+    private final ClaimRuleRepository ruleRepository;
 
-    public ClaimRuleServiceImpl(ClaimRuleRepository repo) {
-        this.repo = repo;
+    public ClaimRuleServiceImpl(ClaimRuleRepository ruleRepository) {
+        this.ruleRepository = ruleRepository;
     }
 
+    @Override
     public ClaimRule addRule(ClaimRule rule) {
-        if (rule.getWeight() < 0)
-            throw new RuntimeException("Weight must be >= 0");
-        return repo.save(rule);
+        if (rule.getWeight() < 0) {
+            throw new BadRequestException("Rule weight must be >= 0");
+        }
+        return ruleRepository.save(rule);
     }
 
+    @Override
     public List<ClaimRule> getAllRules() {
-        return repo.findAll();
+        return ruleRepository.findAll();
     }
 }
